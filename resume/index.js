1bcaa617d0d6526b9f73a0135bba30bc9c1d85cf
MajorDroolz <maximhutz@gmail.com>
2022-12-19 16:37:41 -0500

    FIX: Icons don't cause content layout shifts.

--- resume/index.js
+++
@@ -1,5 +0,0 @@
-window.onload = () => {
-  const links = document.querySelectorAll('link[rel="preload"]');
-  for (const link of links) link.rel = 'stylesheet';
-  document.body.removeAttribute("hidden");
-}
