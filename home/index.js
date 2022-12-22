cf6a5ef9101cbe6efbe7e3bd23aa1160665cbaaf
MajorDroolz <maximhutz@gmail.com>
2022-12-22 16:51:59 -0500

    FEAT: Added rest of unstyled HTML content. Temporarily removed animations.

--- home/index.js
+++ home/index.js
@@ -1,5 +1,3 @@
-let menuOpen = true;
-
 function calcScroll() {
   const scroll = window.scrollY;
   const vh = window.innerHeight / 100;
@@ -12,41 +10,28 @@ function calcScroll() {
 function openMenu() {
   const menu = document.getElementById("menu-modal");
   if (menu == null) return;
-  requestAnimationFrame(() => menuOpen = false);
   menu.showModal();
 }
 
 function closeMenu() {
   const menu = document.getElementById("menu-modal");
   if (menu == null) return;
-  requestAnimationFrame(() => menuOpen = true);
   menu.close();
 }
 
 function main() {
-  document.body.addEventListener('wheel', preventScroll, { passive: false });
-  document.body.addEventListener('touchmove', preventScroll, { passive: false });
   window.addEventListener("scroll", () => requestAnimationFrame(calcScroll));
 
   const menu = document.getElementById("menu-modal");
-
-  if (menu) {
-    menu.addEventListener("click", clickMenuModal);
-    menu.addEventListener("touchstart", clickMenuModal);
-  }
+  if (menu == null) return;
+  menu.addEventListener("click", clickMenuModal);
+  menu.addEventListener("touchstart", clickMenuModal);
 }
 
 function clickMenuModal(e) {
   const menu = document.getElementById("menu-modal");
   if (menu == null || e.target != menu) return;
   closeMenu();
-}
-
-function preventScroll(e) {
-  if (menuOpen) return;
-  e.preventDefault();
-  e.stopPropagation();
-  return false;
 }
 
 window.onload = main;
